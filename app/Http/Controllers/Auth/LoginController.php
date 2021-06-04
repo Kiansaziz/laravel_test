36aa48b78e0fed4344be3cce98115d809af9af1a
kiansaz <mail.kiansaziz@gmail.com>
2021-06-04 17:38:11 +0700

    social first

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use App\Models\User;
+use Socialite;
+use Auth;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    public function redirectToGoogle()
+    {
+       return Socialite::driver('google')->redirect();
+    }
+
+    public function handleGoogleCallBack()
+    {
+       $user = Socialite::driver('google')->user();
+       // $user->token
+       $this->_registerOrLoginUser($user);
+
+       return redirect()->route('home');
+    }
+
+    public function redirectToFacebook()
+    {
+       return Socialite::driver('facebook')->redirect();
+    }
+
+    public function handleFacebookCallBack()
+    {
+       $user = Socialite::driver('facebook')->user();
+       // $user->token
+       $this->_registerOrLoginUser($user);
+
+       return redirect()->route('home');
+    }
+
+    protected function _registerOrLoginUser($data)
+    {
+      $user = User::where('email' , $data->email)->first();
+
+      if (!$user)
+      {
+        $user = new User;
+        $user->name = $data->name;
+        $user->email = $data->email;
+        $user->provider_id = $data->id;
+        $user->avatar = $data->avatar;
+        $user->save();
+      }
+
+      Auth::login($user);
+    }
+}
